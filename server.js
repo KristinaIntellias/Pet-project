ba9e23642aac3cd4c302301adacf1b768c2cf162
KristinaKibalnichenko <k.kibalnichenko@gmail.com>
2022-02-08 13:49:23 +0200

    actions added

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api', apiRouter);
 app.use(express.static(path.join(__dirname, '/dist/kristina-project')));
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/dist/kristina-project');
+  res.sendFile(__dirname + '/dist/kristina-project/index.html');
 });
 
 app.get('*', (req, res, next) => {
